b98b79b59c9761c5d3b3b846d3afede93d14bb86
yik <yik_cyber@yeah.net>
2020-06-13 10:01:35 +0800

    一个等价变形，采用滚动数组

---
+++ POJ/上机汇总/数字组合2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int f[25];
+int num[25];
+int n, t;
+
+int main() {
+    scanf("%d %d", &n, &t);
+    for (int i = 1; i <= n; i++) scanf("%d", &num[i]);
+
+    f[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = t; j >= num[i]; j--) {
+            f[j] += f[j-num[i]];
+        }
+    }
+    printf("%d", f[n]);
+}
