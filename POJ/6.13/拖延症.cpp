d1920e12adccd3c75c758fa7a9de5c13e9a77cc2
yik <yik_cyber@yeah.net>
2020-06-14 11:42:53 +0800

    dfs，还没提交，先放这

---
+++ POJ/6.13/拖延症.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int v[16], s[16];
+bool status[16];
+int tot_s, ans;
+vector<int> before, after;
+
+void dfs(int x, int value, int mental){
+    if (x == 16){
+        if(before.size() <= after.size()) ans = max(ans, value);
+        return;
+    }
+    if (mental < 0) return;
+
+    if(x <= 10){
+        before.push_back(x);
+        dfs(x+1, value+v[x], mental-s[x]);
+        before.pop_back();
+    } else{
+        after.push_back(x);
+        dfs(x+1, value+v[x], mental-s[x]);
+        after.pop_back();
+    }
+    dfs(x+1, value, mental);
+}
+
+int main(){
+    memset(status, false, sizeof(status));
+    scanf("%d", &tot_s);
+    for(int i = 1; i <= 15; i++) scanf("%d %d", &v[i], &s[i]);
+    dfs(1, 0, tot_s);
+    printf("%d", ans);
+}
