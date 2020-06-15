174383716372fb6fe25cda2ffbbf5d2357aebf67
yik <yik_cyber@yeah.net>
2020-06-15 22:13:54 +0800

    把i的范围写成1-n然后卡了我半个小时（我就是春虫虫

--- POJ/上机汇总/炮兵阵地.cpp
+++ POJ/上机汇总/炮兵阵地.cpp
@@ -1,75 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, m;
-char dis[100][10];
-bool valid[100][1<<10];
-int f[100][1<<10][1<<10];
-set<int> S;
+int n, m, ans;
+char dis[110][11];
+int dp[110][110][110];
+int status[110], counts[110], maps[110];
 
 int count_one(int x){
     int cnt = 0;
-    while(x > 0){
+    while (x>0){
         if(x&1) cnt++;
-        x = x>>1;
+        x = x >> 1;
     }
     return cnt;
 }
 int main(){
-    memset(valid, false, sizeof(valid));
     cin >> n >> m;
-    int i, j, k, u;
-    for(i = 0; i < n; i++){
-        for(j = 0; j < m; j++){
-            cin >> dis[i][j];
-        }
-    }
-    int end = 1<<m;
-    int t;
-    int indx;
-    bool in_s;
-    for(i = 0; i < end; i++){
-        indx = -10;
-        in_s = true;
-        for(j = 0; j < m; j++){
-            if((i>>j)&1 && ((i>>j+1)&1 || (i>>j+2)&1)) in_s = false;
-        }
-        if(in_s) S.insert(i);
-    }
-
-    bool val;
     for(int i = 0; i < n; i++){
-        for(auto p = S.begin(); p != S.end(); p++){
-            t = *p;
-            val = true;
-            for(int j = 0; j < m; j++){
-                if((t>>j)&1){
-                    if(dis[i][j] == 'H'){
-                        val = false;
-                        break;
+        int sum = 0;
+        for(int j = 0; j < m; j++){
+            cin >> dis[i][j];
+            if(dis[i][j] == 'P') sum += 1<<(m-j-1);
+        }
+        maps[i] = sum;
+    }
+    int end = 1 << m;
+    int tot = 0;
+    for(int i = 0; i < end; i++){
+        if(i&(i<<1) || i&(i<<2)) continue; //compare with the former number
+        status[tot] = i;
+        counts[tot++] = count_one(i);
+    }
+    for(int i = 0; i < tot; i++){
+        if((maps[0] | status[i]) == maps[0]) dp[0][i][0] = counts[i];
+    }
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j < tot; j++){
+            if((maps[i]|status[j]) == maps[i]){
+                for(int k = 0; k < tot; k++){
+                    if((status[j]&status[k])==0){
+                        for(int l = 0; l < tot; l++){
+                            if((status[l]&status[k]) == 0 && ((status[l]&status[j])==0)){
+                                dp[i][j][k] = max(dp[i][j][k], dp[i-1][k][l]+counts[j]);
+                            }
+                        }
                     }
                 }
             }
-            if(val) valid[i][t] = true;
         }
     }
-    memset(f, -2e8, sizeof(f));
-    f[0][0][0] = 0;
-    int ans = 0;
-    for(i = 0; i < n; i++){
-        for(auto p = S.begin(); p != S.end(); p++){
-            j = *p;
-            if(!valid[i][j]) continue;
-            for(auto q = S.begin(); q != S.end(); q++){
-                k = *q;
-                if(!valid[i-1][k] || !(j&k)) continue;
-                for(auto l = S.begin(); l != S.end(); l++){
-                    u = *l;
-                    if(!valid[i-2][u] || !(k&u)) continue;
-                    f[i][j][k] = max(f[i-1][k][u] + count_one(j), f[i][j][k]);
-
-                }
-                if (i == n-1) ans = max(f[i][j][k], ans);
+    for(int i = 0;i < n; i++){
+        for(int j = 0; j < tot; j++){
+            for(int k = 0; k < tot; k++){
+                ans = max(ans, dp[i][j][k]);
             }
         }
     }
